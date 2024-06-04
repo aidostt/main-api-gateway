4bc05d731f52600fb030afe425b11b5304aee942
Drunck <nur.kons1977@gmail.com>
2024-06-04 10:58:25 -0700

    Updated CORS policy

--- internal/delivery/middleware.go
+++ internal/delivery/middleware.go
@@ -99,7 +99,7 @@ func hasAnyPermittedRole(userRoles []string, permittedRoles []string) bool {
 
 func corsMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, OPTIONS")
 	c.Header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type")
 	c.Header("Content-Type", "application/json")
 	c.Header("Access-Control-Allow-Credentials", "true")
